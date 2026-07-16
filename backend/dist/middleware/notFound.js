051a8a9f0e32f6b2f4599a1ad6bd3097d0d3ef36
Ananya Santra <asantra@ideatarmac.com>
2026-07-16 18:37:47 -0500

    Deleted dist folder

--- backend/dist/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-const envelope_1 = require("../utils/envelope");
-function notFound(req, res) {
-    res.status(404).json((0, envelope_1.fail)(`Route not found ${req.method}`));
-}
